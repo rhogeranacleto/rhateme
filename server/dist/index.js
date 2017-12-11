ee90de4338e78f92a8780454106a2da83b6cea92
Rhoger Anacleto <rhogeranacleto@gmail.com>
2017-12-11 15:49:53 -0200

    Removendo dist

--- server/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./src/Server");
-const Database_1 = require("./src/Database");
-Database_1.dbConnect();
-Server_1.RHateMeServer.run();
-//# sourceMappingURL=index.js.map
